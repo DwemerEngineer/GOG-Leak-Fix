e6dc610077be679facaddd427245d1d119244aea
Anthony <attitus3@gmail.com>
2026-01-09 21:14:04 -0500

    Upload Project Files

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,36 @@
+struct ReplaceRelease
+{
+	static void thunk(RE::BSGraphics::Renderer* renderer, RE::BSGraphics::Texture* tex)
+	{
+		if (_InterlockedExchangeAdd(&tex->unk20, 0xFFFFFFFF) == 1) {
+			if(tex->resourceView)
+				tex->resourceView->Release();
+			if (tex->texture) 
+				tex->texture->Release();
+			if (tex->unk08)
+				reinterpret_cast<ID3D11UnorderedAccessView*>(tex->unk08)->Release();
+
+			REL::Relocation<void (void*, int)> NiMemFee{ RELOCATION_ID(102158, 109588) };
+			NiMemFee(tex, 0x28);
+		}
+
+		return;
+	}
+	static inline REL::Relocation<decltype(thunk)> func;
+
+	static void Install()
+	{
+		stl::write_thunk_jump<ReplaceRelease>(RELOCATION_ID(75527, 77322).address());
+	}
+
+};
+
 SKSEPluginLoad(const SKSE::LoadInterface* a_skse)
 {
 	SKSE::Init(a_skse);
 
-	logs::info("Hello World!");
+	SKSE::AllocTrampoline(14);
+	ReplaceRelease::Install();
 
 	return true;
 }
--- src/pch.h
+++ src/pch.h
@@ -3,5 +3,17 @@
 #include <RE/Skyrim.h>
 #include <SKSE/SKSE.h>
 
+namespace stl
+{
+	using namespace SKSE::stl;
+
+	template <class T>
+	void write_thunk_jump(std::uintptr_t a_src)
+	{
+		auto& trampoline = SKSE::GetTrampoline();
+		T::func = trampoline.write_branch<5>(a_src, T::thunk);
+	}
+}
+
 namespace logs = SKSE::log;
 using namespace std::literals;
